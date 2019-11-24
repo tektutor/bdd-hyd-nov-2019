9c4b45bbe35b7f99735bec1de74814c057098a01
Jeganathan Swaminathan <mail2jegan@gmail.com>
2019-11-24 12:40:50 +0530

    Fixed bug.

--- Day2/savingsaccount/src/test/java/org/tektutor/savingsaccount/TestRunner.java
+++ Day2/savingsaccount/src/test/java/org/tektutor/savingsaccount/TestRunner.java
@@ -1,7 +1,9 @@
 package org.tektutor.savingsaccount;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(features="src/test/java")
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
